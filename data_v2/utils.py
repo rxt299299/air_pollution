25a29c971725acef5ef961653f598f423fd23f0a
rxt299299 <rxt299299@gmail.com>
2023-01-01 17:46:50 -0800

    update data process with update data
    update data process with update data

---
+++ data_v2/utils.py
@@ -0,0 +1,4 @@
+air_pollutions = ['nox','no2','no','o3','pm2.5','pm10']
+weathers = ['ws', 'wd', 'temp', 'RH']
+weathers_2 = ['ssr', 'tp', 'blh', 'tcc', 'sp', 'tc']
+times = ['date_unix', 'week', 'weekday', 'hour', 'month', 'day_julian']
